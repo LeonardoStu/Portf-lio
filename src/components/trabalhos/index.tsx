8a5919a9d6ec75d8822697a09d7731b83cdf70ec
Leonardo Souza <leonardoDevSantos@gmail.com>
2024-06-27 09:39:08 -0300

    Feito a resposifidade da seção de trabalhos

--- src/components/trabalhos/index.tsx
+++ src/components/trabalhos/index.tsx
@@ -1,23 +1,107 @@
-import { Card, CardBody, CardGroup, CardImg, CardText, CardTitle } from 'reactstrap'
+import { Card, CardBody, CardGroup, CardImg, CardText, CardTitle, Collapse } from 'reactstrap'
 import style from './style.module.scss'
 import Link from 'next/link'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Image from 'next/image'
 
 export default function Trabalho() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    const toggle = () => setIsOpen(!isOpen);
     useEffect(() => {
         AOS.init()
     },[])
     return<>
         <div className={style.cardGroup} data-aos="fade-right">
             <div className={style.card}>
-                <Link href='https://onebitflix-rust.vercel.app/'  target='blanck_' className={style.link}>
-                    <Image width={450} height={250} src='/trabalhos/onebitflix.png' alt='Onebitflix' className={style.imgJob}/>
-                    <div>
-                        <h2 className={style.title}>Onebitflix</h2>
-                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quam consequatur placeat unde est culpa sed velit? Aut, iure laborum eos quas minima, asperiores ex expedita itaque reprehenderit ut consequatur!</p>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link} target='blanck_'>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link} target='blanck_'>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
+                    </div>
+                </Link>
+            </div>
+            <div className={style.card}>
+                <Link href='https://onebitflix-rust.vercel.app/' className={style.link}>
+                    <Image width={450} height={200} src='/trabalhos/onebitflix.png' alt='one' className={style.imgJob}/>
+                    <div className={style.information}>
+                        <h1 className={style.title}>Onebitflix</h1>
+                        <p className={style.description}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, itaque consectetur. Incidunt, ducimus quod. Numquam illo maiores dolores ab. Voluptas!</p>
                     </div>
                 </Link>
             </div>
